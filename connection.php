b1eb323432523b66987ce4289e60c0e82ac98f97
Yasho-2005 <145251989+Yasho-2005@users.noreply.github.com>
2024-10-11 20:29:22 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+ <?php
+ $db_name = 'mysql:host=localhost;dbname=shop_db';
+ $db_user = 'root';
+ $db_password = '';
+
+ $conn = new PDO($db_name,$db_user,$db_password);
+
+ if($conn){
+    echo "connected";
+ }
---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+include 'components/connection.php';
+?>
+<style type="text/css">
+    <?php include 'style.css';?>
+</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Green Coffee - home page</title>
+</head>
+<body>
+    <?php include 'components/header.php'; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="script.js"></script>
+    <?php include 'components/alert.php'; ?>
+</body>
+</html>
